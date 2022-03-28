2b06c40b5be4cd90f41819c558950a25ea391ca3
Alex <alexandre_2420@outlook.com>
2022-03-27 19:55:51 -0500

    added: router on App with HOC

--- src/App.js
+++ src/App.js
@@ -1,25 +1,61 @@
-import logo from './logo.svg';
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+  Navigate
+} from 'react-router-dom'
+//firebase config
+import { auth } from './services/firebase'
+//styles
 import './App.css';
+//pages
+import { Home } from './pages/Home'
+import { Chat } from './pages/Chat'
+import { Login } from './pages/Login'
+import { Signup } from './pages/Signup'
+import { useState } from 'react';
 
-function App() {
+
+function PrivateRoute({ component: Component, authenticated, ...rest}) {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <Route
+      { ...rest }
+      render={ (props) => authenticated === true
+        ? <Component { ...props } />
+        : <Navigate replace to={{
+          pathname: '/login', state: { from: props.location}
+        }} />
+      }
+    />
+  )
 }
 
-export default App;
+function PublicRoute({ component: Component, authenticated, ...rest}) {
+  return (
+    <Route
+      { ...rest }
+      render={(props) => authenticated === false
+        ? <Component { ...props } />
+        : <Navigate replace to='/chat'/>
+      }
+    />
+  )
+}
+
+function App() {
+  const [ authenticated, setAuthenticated] = useState(false)
+  const [ loading, setLoading ] = useState(true)
+
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={ <Home />} />
+        <PrivateRoute authenticated={ authenticated } element={ <Chat />} />
+        <PublicRoute authenticated={ authenticated } element={ <Signup />} />
+        <PublicRoute authenticated={ authenticated } element={ <Login />} />
+      </Routes>
+    </BrowserRouter>
+  )
+}
+
+export default App
