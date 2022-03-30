549fff6b7e92c0dce2b3a6e3fd82768805288568
Alex <alexandre_2420@outlook.com>
2022-03-30 14:18:28 -0500

    added: style for Chat page

--- src/pages/Chat.js
+++ src/pages/Chat.js
@@ -2,6 +2,8 @@ import { useEffect, useState } from 'react'
 import { ref, onValue, set } from 'firebase/database'
 //services firebase
 import {  auth, db } from '../services/firebase'
+//styles
+import style from '../styles/pages/Chats.module.css'
 
 export const Chat = () => {
 
@@ -20,9 +22,7 @@ export const Chat = () => {
         const data = snapshot.val()
         setChats(data)
       })
-
     } catch(e) {
-      console.log(e)
       setReadError(e.message)
     }
   }
@@ -59,35 +59,37 @@ export const Chat = () => {
 
 
   return (
-    <main>
-      <form onSubmit={handleSubmit}>
-        <input
-          type="text"
-          name='content'
-          value={content}
-          onChange={handleOnChange}
-        />
-        {
-          writeError && <span>{writeError}</span>
-        }
-        <input type="submit" value="Send" />
-      </form>
+    <main className={ style.Chats }>
+      <div className={ style.Chats_Wrapper }>
+        <form className={ style.Form } onSubmit={handleSubmit}>
+          <input
+            type="text"
+            name='content'
+            value={content}
+            onChange={handleOnChange}
+          />
+          {
+            writeError && <span>{writeError}</span>
+          }
+          <input className={ style.Form_Button } type="submit" value="Send" />
+        </form>
 
-      <section>
-        Login in as: <strong>{user.email}</strong>
-      </section>
+        <section className={ style.User }>
+          Login as: <strong>{user.email}</strong>
+        </section>
 
-      <section>
-        { readError && <span>{readError.message}</span>}
-        { !readError && !chats && <span>Write your first chat!</span>}
-        {
-          chats && Object.values(chats).map(chat => (
-            <p key={chat.timestamp}>
-              { chat.content }
-            </p>
-          ))
-        }
-      </section>
+        <section className={ style.Content }>
+          { readError && <span>{readError.message}</span>}
+          { !readError && !chats && <span>Write your first chat!</span>}
+          {
+            chats && Object.values(chats).map(chat => (
+              <p key={chat.timestamp}>
+                { chat.content }
+              </p>
+            ))
+          }
+        </section>
+      </div>
     </main>
   )
 }
