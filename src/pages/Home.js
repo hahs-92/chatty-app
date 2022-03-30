c9081905b29d3c4c80bb5fbe88265f0b7f888ffa
Alex <alexandre_2420@outlook.com>
2022-03-30 14:55:49 -0500

    added: styles for Home page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,15 @@
-import React from 'react'
+//assets
+import bg from '../assets/pexels-freestocksorg-744464 (1).jpg'
+//styles
+import style from '../styles/pages/Home.module.css'
 
 export const Home = () => {
   return (
-    <div>Home</div>
+
+    <main className={ style.Home }>
+      <section className={ style.Image }>
+        <img src={ bg } alt="background" />
+      </section>
+    </main>
   )
 }
